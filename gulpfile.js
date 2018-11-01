27754fc1e0a93c34d458d919e0e8f8908592735e
Revi <oliiv@outlook.com>
2018-11-01 11:41:27 -0300

    React con routing nativo(No recomendable para produccion)

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); // Runs a local dev server
+var open = require('gulp-open'); // Open a URL in a web browser
+
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify'); // Transforms React JSX to JS
+var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
+
+var concat = require('gulp-concat'); // Concatenates files
+
+var lint = require('gulp-eslint'); // Lint our js files, including JSX
+// eslint src json = https://github.com/coryhouse/react-flux-starter-kit/blob/master/eslint.config.json
+
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        js: '/.src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
+        dist: './dist',
+        mainJs: './src/main.js'
+    }
+}
+
+// Start a local development server
+gulp.task('connect', function () {
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    })
+});
+
+
+// Will open a keep in file
+gulp.task('open', ['connect'], function () {
+    gulp.src('dist/index.html')
+        .pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
+
+// Handle our html file and get the mover over dist
+gulp.task('html', function () {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+// JS task - browserify
+gulp.task('js', function () {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+// CSS task
+gulp.task('css', function () {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+})
+
+// EsLint task
+gulp.task('lint', function () {
+    return gulp.src(config.paths.js)
+                .pipe(lint({config: 'eslint.config.json'}))
+                .pipe(lint.format());
+});
+
+// task to watch files
+gulp.task('watch', function () {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+    
+});
+
+
+// Task default to run html and open
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
