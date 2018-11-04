4a3992577eedad74dd2346d548b2f792f6cf2949
Revi <oliiv@outlook.com>
2018-11-04 19:23:48 -0300

    wrap all - React Routing declared - params and querys - Normalized anchors via Link - handing transitions

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
+var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
+
+var routes = (
+    <Route name="app" path="/" handler={require('./components/app')} >
+        <DefaultRoute handler={require('./components/homePage')} />
+        <NotFoundRoute handler={require('./components/notFoudPage')} />
+        <Route name="authors" handler={require('./components/authors/authorPage')} />
+        <Route name="about" handler={require('./components/about/aboutPage')} />
+        <Redirect from="awthors/*" to="authors" />
+        <Redirect from="about/*" to="about" />
+    </Route>
+);
+
+module.exports = routes;
