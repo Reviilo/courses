5522e49de70ac5a3d7ce4d44564d4bc63d962408
Revi <oliiv@outlook.com>
2018-11-06 11:30:07 -0300

    Agregar validacion del formulario

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -5,6 +5,8 @@ var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
 
+var toastr = require('toastr');
+
 var manageAuthorPage = React.createClass({
     mixins: [
         Router.Navigation
@@ -12,7 +14,8 @@ var manageAuthorPage = React.createClass({
 
     getInitialState: function () {
         return {
-            author: {id: '', firstName: '', lastName: ''}
+            author: {id: '', firstName: '', lastName: ''},
+            errors: {}
         };
     },
 
@@ -23,9 +26,33 @@ var manageAuthorPage = React.createClass({
         return this.setState({author: this.state.author});
     },
 
+    authorFormIsValid: function () {
+        var formIsValid = true;
+        this.state.errors = {}; //Clear any previous errors.
+
+        if (this.state.author.firstName.length < 3) {
+            this.state.errors.firstName = 'First name must be at least 3 characters.';
+            formIsValid = false;
+        }
+
+        if (this.state.author.lastName.length < 3) {
+            this.state.errors.lastName = 'First name must be at least 3 characters.';
+            formIsValid = false;
+        }
+
+        this.setState({errors: this.state.errors});
+        return formIsValid;
+    },
+
     saveAuthor: function (event) {
         event.preventDefault();
+
+        if (!this.authorFormIsValid()) {
+            return;
+        }
+
         AuthorApi.saveAuthor(this.state.author);
+        toastr.success('Author saved.');
         this.transitionTo('authors');
     },
 
@@ -35,7 +62,8 @@ var manageAuthorPage = React.createClass({
                 <AuthorForm 
                     author={this.state.author} 
                     onChange={this.setAuthorState}
-                    onSave={this.saveAuthor} />
+                    onSave={this.saveAuthor}
+                    errors={this.state.errors} />
             </div>
         );
     }
